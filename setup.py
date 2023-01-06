9eb0d4b67f81994047f4733053dead823741b0c1
Alessandro Pagiaro <alessandropagiaro@gmail.com>
2023-01-06 10:27:33 +0100

    Improve metadata

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import find_packages, setup
 setup(
     name='react_context',
     packages=find_packages(include=['react_context']),
-    version='0.2.0',
+    version='0.3.0',
     description='An API to pass data in the sub-function inspired by React Context',
-    author='Alessandro Pagiaro <alessandropagiaro@gmail.com>',
+    author='Alessandro Pagiaro',
+    author_email='alessandropagiaro@gmail.com',
+    url='https://github.com/alessandro308/react_context',
     license='MIT',
     install_requires=[],
+    readme="README.md",
     tests_require=['pytest==7.2.0'],
     test_suite='tests',
 )
