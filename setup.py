f9db99ab766eabe89e28d3f3c2a5ef684a813a25
Alessandro Pagiaro <alessandropagiaro@gmail.com>
2023-01-06 10:30:24 +0100

    Add long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from setuptools import find_packages, setup
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
 setup(
     name='react_context',
     packages=find_packages(include=['react_context']),
-    version='0.3.0',
+    version='0.4.0',
     description='An API to pass data in the sub-function inspired by React Context',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Alessandro Pagiaro',
     author_email='alessandropagiaro@gmail.com',
     url='https://github.com/alessandro308/react_context',
